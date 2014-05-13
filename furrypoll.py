d47b0c9d592952370c99a3d6c5cebd8e3c3dff18
Matthew Scott <matthew.scott@canonical.com>
2014-05-13 11:55:55 -0600

    Further work - models work and save and such.

--- furrypoll.py
+++ furrypoll.py
@@ -3,45 +3,114 @@ from flask import (
     g,
     session,
     render_template,
+    request,
 )
 from flask.ext.mongoengine import MongoEngine
 
 import os
+import datetime
+
+import models
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': 'furrypoll_2014'}
 app.config["SECRET_KEY"] = os.urandom(12)
+app.config["DEBUG"] = True
 
 db = MongoEngine(app)
 
 @app.route('/')
 def front():
-    return render_template('templates/front.html')
+    return render_template('front.html')
 
 @app.route('/survey/start/')
 def surveyStart():
     # create document, store id, survey start touchpoint, store ip, ua
-    pass
+    survey = models.Response()
+    survey.metadata = models.ResponseMetadata(
+        client_ip=request.remote_addr,
+        client_ua=str(request.user_agent)
+    )
+    start_tp = models.Touchpoint(
+        touchpoint_type=0
+    )
+    survey.metadata.touchpoints.append(start_tp)
+    survey.save()
+    session['response_id'] = str(survey.id)
+    return render_template('start.html')
+
+@app.route('/touch/question/<int:question_id>')
+def surveyQuestion(question_id):
+    # store a touchpoint that a question was answered
+    tp = models.Touchpoint(
+        touchpoint_type=question_id
+    )
+    survey = models.Response.objects.get(id=session['response_id'])
+    survey.metadata.touchpoints.append(tp)
+    survey.save()
+    return '{error:null}'
 
 @app.route('/survey/overview/')
 def surveyOverview():
-    pass
+    survey = models.Response.objects.get(id=session['response_id'])
+    if request.method == 'POST':
+        tp = models.Touchpoint(
+            toucpoint_type=-1
+        )
+        survey.metadata.touchpoints.append(tp)
+        # save answers
+        survey.save()
+        # redirect to psychographic
+    else:
+        return render_template('overview.html')
 
 @app.route('/survey/psychographic/')
 def surveyPsychographic():
-    pass
+    survey = models.Response.objects.get(id=session['response_id'])
+    if request.method == 'POST':
+        tp = models.Touchpoint(
+            toucpoint_type=-2
+        )
+        survey.metadata.touchpoints.append(tp)
+        # save answers
+        survey.save()
+        # redirect to sexuality
+    else:
+        return render_template('psychographic.html')
 
 @app.route('/survey/sexuality/')
 def surveySexuality():
-    pass
+    survey = models.Response.objects.get(id=session['response_id'])
+    if request.method == 'POST':
+        tp = models.Touchpoint(
+            toucpoint_type=-3
+        )
+        survey.metadata.touchpoints.append(tp)
+        # save answers
+        survey.save()
+        # redirect to sexuality
+    else:
+        return render_template('psychographic.html')
 
 @app.route('/survey/complete/')
 def surveyComplete():
-    pass
+    survey = models.Response.objects.get(id=session['response_id'])
+    tp = models.Touchpoint(
+        touchpoint_type=-4
+    )
+    survey.metadata.touchpoints.append(tp)
+    survey.save()
+    return render_template('complete.html')
 
 @app.route('/survey/cancel/')
 def surveyCancel():
-    pass
+    survey = models.Response.objects.get(id=session['response_id'])
+    tp = models.Touchpoint(
+        touchpoint_type=-5
+    )
+    survey.metadata.touchpoints.append(tp)
+    survey.save()
+    return render_template('cancel.html')
 
 if __name__ == '__main__':
     app.secret_key = 'Development key'
--- models.py
+++ models.py
@@ -1,64 +1,18 @@
 import datetime
 from furrypoll import db
 
-class Response(db.Document):
-    survey_id = db.StringField(max_length=255, required=True)
-    overview = db.EmbeddedDocumentField(Overview)
-    psychographic_battery = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    sexuality = db.EmbeddedDocumentField(Sexuality)
-    metadata = db.EmbeddedDocumentField(ResponseMetadata)
+class Touchpoint(db.EmbeddedDocument):
+    """ Store a touchpoint of a user's interaction with the survey
 
-class Overview(db.EmbeddedDocument):
-    birth_month = db.IntField()
-    birth_year = db.IntField()
-    clinical_sex = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    gender_identity = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    gender_identity_coords = db.EmbeddedDocumentField(GenderIdentityCoordinates)
-    sexual_orientation = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    country = db.StringField(max_length=2, default='xx')
-    state_province = db.StringField(max_length=2, default='xx')
-    location_size = db.StringField(max_length=20)
-    race = db.ListField(db.EmbeddedDocumentField(PotentiallySubjectiveResponse))
-    spirituality = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    political_views = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    occupation = db.ListField(db.EmbeddedDocumentField(PotentiallySubjectiveResponse))
-    education = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    relationship = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    partner_is_furry = db.BooleanField()
-    long_distance_relationship = db.BooleanField()
-    how_furry = db.IntField()
-    how_long_known = db.FloatField()
-    how_long_member = db.FloatField()
-    who_knows = db.StringField(max_length=30)
-    non_furries_general_response = db.StringField(max_length=30)
-    non_furries_personal_response = db.StringField(max_length=30)
-    non_furries_perception_accuracy = db.StringField(max_length=30)
-    how_human = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-    gender_in_furry = db.StringField(max_length=30)
-    gender_in_furry_coords = db.EmbeddedDocumentField(GenderIdentityCoordinates)
-    furry_activities = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    furry_activities_opinion = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    non_furry_activities = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    self_described = db.ListField(db.StringField(max_length=30))
-    furry_importance = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    conventions = db.ListField(db.StringField(max_length=30))
-    furry_websites = db.ListField(db.EmbeddedDocumentField(StringPerOption))
-    characters = db.ListField(db.EmbeddedDocumentField(Character))
-
-class GenderIdentityCoordinates(db.EmbeddedDocument):
-    male = db.FloatField()
-    female = db.FloatField()
-    male_quantized = db.IntField()
-    female_quantized = db.IntField()
-
-class Character(db.EmbeddedDocument):
-    species = db.ListField(db.EmbeddedDocumentField(PotentiallySubjectiveResponse))
-    reason = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
-
-class Sexuality(db.EmbeddedDocument):
-    sex_importance = db.ListField(db.EmbeddedDocumentField(NumberPerOption))
-    interests = db.ListField(db.EmbeddedDocumentField(StringPerOption))
-    other_interests = db.EmbeddedDocumentField(PotentiallySubjectiveResponse)
+    touchpoint_type: One of the following:
+        0 - survey created
+        -1, -2, -3 - Answers to page 1, 2, or 3
+        -4 - survey completed
+        -5 - survey cancelled
+        1...n - answers to question 1...n
+    """
+    touchpoint_type = db.IntField()
+    timestamp = db.DateTimeField(default=datetime.datetime.now, required=True)
 
 class NumberPerOption(db.EmbeddedDocument):
     option = db.StringField(max_length=30)
@@ -72,11 +26,71 @@ class PotentiallySubjectiveResponse(db.EmbeddedDocument):
     subjective = db.BooleanField()
     value = db.StringField(max_length=2000)
 
+class GenderIdentityCoordinates(db.EmbeddedDocument):
+    male = db.FloatField()
+    female = db.FloatField()
+    male_quantized = db.IntField()
+    female_quantized = db.IntField()
+
+class Character(db.EmbeddedDocument):
+    species = db.ListField(db.EmbeddedDocumentField('PotentiallySubjectiveResponse'))
+    reason = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+
+class Sexuality(db.EmbeddedDocument):
+    sex_importance = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    interests = db.ListField(db.EmbeddedDocumentField('StringPerOption'))
+    other_interests = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+
 class ResponseMetadata(db.EmbeddedDocument):
     client_ip = db.StringField(max_length=16, required=True)
     client_ua = db.StringField(required=True)
-    touchpoints = db.ListField(db.EmbeddedDocumentField(Touchpoint))
+    touchpoints = db.ListField(db.EmbeddedDocumentField('Touchpoint'))
 
-class Touchpoint(db.EmbeddedDocument):
-    touchpoint_type = db.StringField(max_length=1) # (c)reated, (r)esponse to page, (s)ubmit, (x)canceled.
-    timestamp = db.DateTimeField(default=datetime.datetime.now, required=True)
+class Overview(db.EmbeddedDocument):
+    birth_month = db.IntField()
+    birth_year = db.IntField()
+    clinical_sex = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    gender_identity = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    gender_identity_coords = db.EmbeddedDocumentField('GenderIdentityCoordinates')
+    sexual_orientation = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    country = db.StringField(max_length=2, default='xx')
+    state_province = db.StringField(max_length=2, default='xx')
+    location_size = db.StringField(max_length=20)
+    race = db.ListField(db.EmbeddedDocumentField('PotentiallySubjectiveResponse'))
+    spirituality = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    political_views = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    occupation = db.ListField(db.EmbeddedDocumentField('PotentiallySubjectiveResponse'))
+    education = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    relationship = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    partner_is_furry = db.BooleanField()
+    long_distance_relationship = db.BooleanField()
+    how_furry = db.IntField()
+    how_long_known = db.FloatField()
+    how_long_member = db.FloatField()
+    who_knows = db.StringField(max_length=30)
+    non_furries_general_response = db.StringField(max_length=30)
+    non_furries_personal_response = db.StringField(max_length=30)
+    non_furries_perception_accuracy = db.StringField(max_length=30)
+    how_human = db.EmbeddedDocumentField('PotentiallySubjectiveResponse')
+    gender_in_furry = db.StringField(max_length=30)
+    gender_in_furry_coords = db.EmbeddedDocumentField('GenderIdentityCoordinates')
+    furry_activities = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    furry_activities_opinion = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    non_furry_activities = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    self_described = db.ListField(db.StringField(max_length=30))
+    furry_importance = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    conventions = db.ListField(db.StringField(max_length=30))
+    furry_websites = db.ListField(db.EmbeddedDocumentField('StringPerOption'))
+    characters = db.ListField(db.EmbeddedDocumentField('Character'))
+
+class Response(db.Document):
+    # survey_id = db.StringField(max_length=255, required=True)
+    overview = db.EmbeddedDocumentField('Overview')
+    psychographic_battery = db.ListField(db.EmbeddedDocumentField('NumberPerOption'))
+    sexuality = db.EmbeddedDocumentField('Sexuality')
+    metadata = db.EmbeddedDocumentField('ResponseMetadata')
+
+    meta = {
+        'allow_inheritance': True,
+        'collection': 'responses'
+    }
